c715d320e529a3a66f2ee340fbc4a4b1cb2c617f
gt-dongka <denny.ong.cpf@gmail.com>
2018-09-26 16:48:09 +0800

    start_app_server.sh

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://root:passw0rd@ds115283.mlab.com:15283/mlabdb
+#export MONGODB_CREDENTIALS=mongodb://root:passw0rd@ds115283.mlab.com:15283/mlabdb
 
 # finally, run the app in dev mode...
 npm run dev
